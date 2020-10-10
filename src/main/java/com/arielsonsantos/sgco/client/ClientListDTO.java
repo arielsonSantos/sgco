7e02cbe96563e3adf43ccd0c2e3b1e1dbf977260
arielsonSantos <arielsonelias@gmail.com>
2020-10-09 21:20:43 -0300

    Corrigido DTO do cliente

--- src/main/java/com/arielsonsantos/sgco/client/ClientListDTO.java
+++ src/main/java/com/arielsonsantos/sgco/client/ClientListDTO.java
@@ -1,7 +1,12 @@
 package com.arielsonsantos.sgco.client;
 
+import com.arielsonsantos.sgco.address.Address;
+import com.arielsonsantos.sgco.phone.Phone;
+
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ClientListDTO implements Serializable {
 
@@ -14,6 +19,8 @@ public class ClientListDTO implements Serializable {
     private String email;
     private ClientStatus status;
     private String observacoes;
+    private Set<Address> addresses = new HashSet<>();
+    private Set<Phone> phones = new HashSet<>();
 
     public ClientListDTO() {
     }
@@ -28,6 +35,8 @@ public class ClientListDTO implements Serializable {
         this.email = client.getEmail();
         this.status = client.getStatus();
         this.observacoes = client.getObservacoes();
+        this.addresses = client.getAddresses();
+        this.phones = client.getPhones();
     }
 
     public Integer getId() {
@@ -64,5 +73,13 @@ public class ClientListDTO implements Serializable {
 
     public String getObservacoes() {
         return observacoes;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public Set<Phone> getPhones() {
+        return phones;
     }
 }
